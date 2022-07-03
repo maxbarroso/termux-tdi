9206682f5abf75add0b7363137cae4520e44419a
Lyanna <52431428+LyannaValerie@users.noreply.github.com>
2022-07-03 18:31:23 -0400

    Update termuxtdi.sh

--- termuxtdi.sh
+++ termuxtdi.sh
@@ -80,6 +80,7 @@ cd ~
 
 
 echo -e "\e[1;92m Utilizando de um script gigante para você não ter nenhum erro sequer!"
+echo -e "\e[1;92m Isso será bem rápido!"
 
 
 sed -i '13,15 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb; sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb; sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb; sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
